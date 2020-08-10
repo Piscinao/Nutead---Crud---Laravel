42ac45c619d0f3abb94bf0e88facb0e3e3b65685
Piscinao <henhvj@gmail.com>
2020-08-10 15:51:20 -0300

    First Commit

---
+++ resources/views/produtos/formulario_criacao.blade.php
@@ -0,0 +1,27 @@
+<html>
+
+  <head>
+      <title>Formulário Cadastro Produto</title>
+
+  </head>
+
+
+  <body>
+      <form action="" method="post">
+          <label for="">Nome:</label>
+          <input type="text"><br />
+          <label for="">Custo:</label>
+          <input type="text"><br />
+          <label for="">Preço</label>
+          <input type="text"><br />
+          <label for="">Quantidade</label>
+          <input type="text"><br />
+          <label for=""></label>
+          <input type="text"><br />
+          <button>Salvar</button>
+
+      </form>
+  </body>
+
+</html>
+
